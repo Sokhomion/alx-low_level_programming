5e016ffd272bb18258fa5a4dbfd5776b6547e1c3
Sokhomion <ebhodaghesolomon@gmail.com>
2022-12-15 15:46:35 -0800

    fixed some more error

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,40 +3,21 @@
 
 
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always returns 0
- *
- *     *
- *
- *      
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0      
  */
 
 int main(void)
-
 {
-
-		_putchar('_');
-
-			_putchar('p');
-
-				_putchar('u');
-
-					_putchar('t');
-
-						_putchar('c');
-
-							_putchar('h');
-
-								_putchar('a');
-
-									_putchar('r');
-
-											_putchar('\n');
-
-												return (0);
-
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');							
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
